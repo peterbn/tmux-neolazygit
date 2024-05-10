9a337dcc535323623c02fa056022cf9efc1456ff
Peter Birk Pakkenberg <pbirk@google.com>
2024-05-10 11:06:40 +0000

    Make popup option configurable

--- scripts/open-lazygit.sh
+++ scripts/open-lazygit.sh
@@ -1,16 +1,20 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$CURRENT_DIR/helpers.sh"
+
 CUSTOM_LAZYGIT_CONFIG="$CURRENT_DIR/../lazygit/config.yml"
 
 LAZYGIT_EDITOR="$CURRENT_DIR/editor.sh" # Check usage in lazygit/config.yml
 LAZYGIT_CONFIG=$(echo "$(lazygit -cd)/config.yml")
 
+tmux_option_use_popup="@open-lazygit-in-popup"
+
 openLazygit () {
     # Gets the pane id from where the script was called
     local LAZYGIT_ORIGIN_PANE=($(tmux display-message -p "#D"))
-    local USE_POPUP=true # true
+    local USE_POPUP=$(get_tmux_option "$tmux_option_use_popup" "")
 
     # Opens a new tmux window running lazygit appending the needed config
-    if [ $USE_POPUP="true" ];
+    if ! [ -z $USE_POPUP ];
     then
       tmux popup \
           -d "#{pane_current_path}" \
